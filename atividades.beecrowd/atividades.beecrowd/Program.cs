646d28490dd0210fba043a7be2c2717b8b60f09d
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-04-03 19:48:19 -0300

    Add projeto da atividade 1003

--- atividades.beecrowd/atividades.beecrowd/Program.cs
+++ atividades.beecrowd/atividades.beecrowd/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Globalization;
+
+decimal raio = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+decimal pi = 3.14159M;
+decimal area = pi * (raio * raio);
+Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+Console.ReadKey();
