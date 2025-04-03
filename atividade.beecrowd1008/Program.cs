4090d5ef04906dea611309453701222370744d79
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-04-03 20:36:12 -0300

    Add exercicio 1008 pronto

---
+++ atividade.beecrowd1008/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+int numeroFuncionario = int.Parse(Console.ReadLine());
+int numeroHorasTrabalhadas = int.Parse(Console.ReadLine());
+double salarioPorHora= double.Parse(Console.ReadLine());
+double SALARY = numeroHorasTrabalhadas * salarioPorHora;
+Console.WriteLine("NUMBER = " + numeroFuncionario);
+Console.WriteLine("SALARY = U$ " + SALARY.ToString("F2", CultureInfo.InvariantCulture));
+Console.ReadKey();
