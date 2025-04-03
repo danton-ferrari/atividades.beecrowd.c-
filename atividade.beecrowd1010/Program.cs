9d24b9e44b1cc0a4b132b1881ddeee6387837496
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-04-03 20:52:21 -0300

    Add exercicio 1010 pronto

--- atividade.beecrowd1010/Program.cs
+++ atividade.beecrowd1010/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] linha = Console.ReadLine().Split(' ');
+string codigo1 = linha[0];
+int numeroPecas1 = int.Parse(linha[1]);
+decimal valorUnitario1 = decimal.Parse(linha[2]);
+
+linha = Console.ReadLine().Split(' ');
+string codigo2 = linha[0];
+int numeroPecas2 = int.Parse(linha[1]);
+decimal valorUnitario2 = decimal.Parse(linha[2]);
+
+decimal total = (numeroPecas1 * valorUnitario1) + (numeroPecas2 * valorUnitario2);
+Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2"));
+Console.ReadKey();
