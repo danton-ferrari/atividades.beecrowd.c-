96e93b7030f74e9f3e0264f5da1dfeb351614b04
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-04-03 21:08:01 -0300

    Add exercicio 1016 pronto

--- atividade.beecrowd1016/Program.cs
+++ atividade.beecrowd1016/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numero = int.Parse(Console.ReadLine());
+Console.WriteLine((numero*2)+" minutos");
+Console.ReadKey();
