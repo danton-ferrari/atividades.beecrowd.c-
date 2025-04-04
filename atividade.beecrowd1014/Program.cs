2b96b007ca2d22fcc1b7c6e817c614f384bb2099
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-04-03 21:02:37 -0300

    Add exercicio 1014 pronto

--- atividade.beecrowd1014/Program.cs
+++ atividade.beecrowd1014/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+int x = int.Parse(Console.ReadLine());
+decimal y = decimal.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+decimal consumeMedio = x / y;
+Console.WriteLine(consumeMedio.ToString("F3")+" km/l");
+Console.ReadKey();
